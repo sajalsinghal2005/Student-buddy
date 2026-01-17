07b3a18abd22448e0992d7218b63e99c73741a03
Aslamkhann27 <aslamkhan809442@gmail.com>
2026-01-18 02:20:32 +0530

    Remove Suspense wrapper from Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
 import AppLayout from '@/components/app-layout';
-import { Suspense } from 'react';
 
 export default function Home() {
-  return (
-    <Suspense>
-      <AppLayout />
-    </Suspense>
-  );
+  return <AppLayout />;
 }
